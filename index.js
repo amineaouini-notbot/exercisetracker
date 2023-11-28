76de8e52bc6bbc2d6d3d7464b61c554a2c65f0b4
Geekyume <aminaouini565@gmail.com>
2023-11-28 21:34:11 +0100

    connect mongodb & fixed parsed incoming data

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const {json, urlencoded} = require('body-parser')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/exercisetracker')
+.then(()=>{ console.log('DB is connected!!')})
+.catch(err=> {throw err})
 
 app.use(cors())
+// parse incoming data
+app.use(json()) 
+app.use(urlencoded({ extended: false }))
+
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+const usersRouter = require('./routes/users')
 
-
+app.use('/api/users', usersRouter)
 
 
 const listener = app.listen(3000, () => {
