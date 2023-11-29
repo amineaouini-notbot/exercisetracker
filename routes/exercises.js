5d04282ae58438de9fe07b7b14b65ccc03c785b3
Geekyume <aminaouini565@gmail.com>
2023-11-29 23:35:46 +0100

    finalize get user logs request

--- routes/exercises.js
+++ routes/exercises.js
@@ -7,15 +7,17 @@ router.post('/:_id/exercises', async (req, res) => {
     let { _id } = req.params
     let {description, duration, date} = req.body;
     dateSetup = new Date(date)
-    date =  dateSetup.toString() === 'Invalid Date' ? new Date() : dateSetup
+    date =  dateSetup.toString() === 'Invalid Date' ?
+        new Date().toDateString() :
+        dateSetup.toDateString()
     
     try {
-        let newExercise = new Exercises({description, duration, date})
+        let newExercise = new Exercises({description, duration, date: date})
         await newExercise.save()
         let user = await Users.findByIdAndUpdate(_id, {$push: newExercise._id})
         let { username } = user;
         let response = {
-            _id, username, date: date.toDateString(), duration, description
+            _id, username, date: date, duration, description
         }
         res.json(response)
         
@@ -23,7 +25,23 @@ router.post('/:_id/exercises', async (req, res) => {
 })  
 
 router.get('/:_id/logs', (req, res) =>{
+    let {_id} = req.params;
+    Users.findOne({_id})
+    .then(user => {
+        let count = user.exercises.length
+        let {username, exercises} = user;
 
+        Exercises.find({_id: exercises}).select('description duration date')
+        .then(logs =>{
+            let response = {
+                _id, username,count, logs 
+            }
+            res.json(response);
+            
+        })
+        .catch(err => res.send(err))
+    })
+    .catch(err => res.send(err))
 })
 
 module.exports = router;
