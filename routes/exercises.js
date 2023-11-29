fd3abe23ceef982d6cda632779e7f5be9c72975e
Geekyume <aminaouini565@gmail.com>
2023-11-29 23:09:13 +0100

    fix bug

--- routes/exercises.js
+++ routes/exercises.js
@@ -8,18 +8,22 @@ router.post('/:_id/exercises', async (req, res) => {
     let {description, duration, date} = req.body;
     dateSetup = new Date(date)
     date =  dateSetup.toString() === 'Invalid Date' ? new Date() : dateSetup
-    console.log(date)
+    
     try {
         let newExercise = new Exercises({description, duration, date})
         await newExercise.save()
         let user = await Users.findByIdAndUpdate(_id, {$push: newExercise._id})
         let { username } = user;
         let response = {
-            _id, username, date, duration, description
+            _id, username, date: date.toDateString(), duration, description
         }
         res.json(response)
         
     } catch(err) { res.json(err)}
 })  
+
+router.get('/:_id/logs', (req, res) =>{
+
+})
 
 module.exports = router;
